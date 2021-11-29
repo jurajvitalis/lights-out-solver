71c5d4f411bd919acdb3a5febd067366704ff24d
jurajvitalis <62528806+jurajvitalis@users.noreply.github.com>
2021-11-29 23:43:14 +0100

    refactor algos import Node from separate file

--- greedy.py
+++ greedy.py
@@ -1,96 +1,48 @@
-import numpy as np
-
 import constants
+import heapq
+from graph_node import Node
+
+from PyQt5 import QtWidgets
 
 
-class Node:
-    def __init__(self, stateLights: np.ndarray, stateSwitches: np.ndarray, parent, action: tuple):
-        self.stateLights = stateLights
-        self.stateSwitches = stateSwitches
-        self.parent = parent
-        self.action = action
+def greedySolve2(startNode: Node, board: QtWidgets.QWidget, render: bool) -> list:
 
-    def isSolved(self) -> bool:
-        if self.stateLights.sum() == 0:
-            return True
-        return False
+    prioQueue = []
+    heapq.heappush(prioQueue, (startNode.hVal, startNode))
+    marked = []
+    expanded_nodes = 0
 
-    def move(self, row: int, col: int) -> None:
-        n_rows, n_cols = self.stateLights.shape
-        if row < 0 or row >= n_rows or col < 0 or col >= n_cols:
-            return
+    while len(prioQueue) > 0:
 
-        # Zapis action
-        self.action = (row, col)
+        # Vyberie posledne pridany node
+        node = heapq.heappop(prioQueue)[1]
 
-        # Zaznamenaj zmenu v matici stateSwitches
-        self.stateSwitches[row, col] = 1
+        if node.isSolved():
+            if render:
+                board.renderState(node.stateLights, node.stateSwitches, constants.RENDER_STATE_MS)
+                print(node.stateSwitches)
 
-        # Zaznamenaj zmenu v matici stateLights
-        for (r, c) in ((row, col), (row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)):
-            if 0 <= r < n_rows and 0 <= c < n_cols:
-                if self.stateLights[r][c]:
-                    self.stateLights[r][c] = False
-                else:
-                    self.stateLights[r][c] = True
+            actions = []
+            while node.parent is not None:
+                actions.append(node.action)
+                node = node.parent
 
-    def greedy(self, tiles, board) -> list:
+            actions.reverse()
+            print(f'Pocet expandovanych uzlov: {expanded_nodes}')
+            return actions
 
-        listOfTiles = [tiles]
-        finalList = []
-        board = board
-        explored = []
+        # Ak node este nebol expandovany, expanduj ho
+        if not(node.stateLights.tolist() in marked):
 
-        while not self.isSolved():
+            print(f'hVal = {node.hVal}', f'pathCost = {node.pathCost}')
+            expanded_nodes += 1
 
-            correctRow = -1  # tile s ktorou budeme hrat
-            correctCol = -1
-            numberOfLights = 100  # jej cislo svietiacich tiles
+            if render:
+                board.renderState(node.stateLights, node.stateSwitches, constants.RENDER_STATE_MS)
 
-            x = 0
-            if len(listOfTiles) == 2:
-                x = 3
-            else:
-                x = 5
+            marked.append(node.stateLights.tolist())
 
-            # cyklus na prejdenie 2d pola s tiles
-            for i in range(len(listOfTiles[0])):
-                for j in range(x):
-                    # vykonanie zapnutia tile
-                    self.move(i, j)
-                    # porovnanie s predchadajucou
-                    if self.stateLights.sum() < numberOfLights:
-
-                        numberOfLights = self.stateLights.sum()
-
-                        # ak po nej ostane menej svietiacich, stava sa correct tile a zapiseme kde je v poli
-                        correctRow = i
-                        correctCol = j
-
-                    # vratenie do povodneho stavu
-                    self.move(i, j)
-
-            # ked prejde vsetky, pozname uz correct tile a mozme s nou urobit zmenu
-
-            if correctRow != -1 and correctCol != -1:
-
-                self.move(correctRow, correctCol)
-                correctTile = (correctRow, correctCol)
-
-                board.algoRender(correctTile, 500)
-
-                finalList.append(correctTile)
-                explored.append(correctTile)
-
-            if len(explored) == pow(len(listOfTiles[0]), x):
-                explored = []
-
-        return finalList
-
-
-# if __name__ == '__main__':
-#     starttNode = Node(stateLights=constants.patterns3[0], stateSwitches=np.zeros((5, 5), int), parent=None, action=None)
-#     solution = starttNode.greedy(starttNode.stateLights)
-#
-#     print('Solution\n')
-#     print(f'moves: {solution[0]}')
+            # Prida susedne nody do queue
+            for new_node in node.getAdjacentNodes():
+                if not (new_node.stateLights.tolist() in marked):
+                    heapq.heappush(prioQueue, (new_node.hVal, new_node))
